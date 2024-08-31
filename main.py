bbf85a86d09ee60274bc41f6a8f579c887a18b00
AbhigyaKrishna <abhigyakrishna21@gmail.com>
2024-09-01 04:53:57 +0530

    feat: speech to sign conversion

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from manager import ConnectionManager
 from pydantic import BaseModel
+import boto3
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 app = FastAPI()
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('voicevideo')
+items = [item.key.split('.')[0] for item in bucket.objects.all()]
+url = os.getenv('AWS_BUCKET_URL')
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 manager = ConnectionManager()
-clients = {}
+
 
 @app.websocket("/video")
 async def video_websocket(ws: WebSocket):
@@ -19,9 +30,18 @@ async def video_websocket(ws: WebSocket):
     except WebSocketDisconnect:
         manager.disconnect(websocket)
 
+
 class Req(BaseModel):
     text: str
 
+
 @app.post("/translate")
 async def translate(body: Req):
-    
+    tokens = body.text.split(" ")
+    tokens = [word if word in items else 'acting' for word in tokens]
+    urls = {word: url + word + '.mp4' for word in tokens}
+    return {
+        'text': ' '.join(tokens),
+        'urls': urls
+    }
+
